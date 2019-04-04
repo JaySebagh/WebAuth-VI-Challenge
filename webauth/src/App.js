fb5557c9edf317f393c0c10a1b7a2d95e0dee910
JaySebagh <jaysebagh@gmail.com>
2019-04-04 15:06:33 -0700

    added logout

--- webauth/src/App.js
+++ webauth/src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
           <NavLink to="/login">Login</NavLink>
           &nbsp;|&nbsp;
           <NavLink to="/users">Users</NavLink>
+          &nbsp;|&nbsp;
+          <button onClick={this.logout}>Logout</button>
         </header>
         <main>
           <Route path="/" exact component={Home} />
@@ -23,6 +25,11 @@ class App extends Component {
         </main>
       </>
     );
+  }
+
+  logout = () => {
+    localStorage.removeItem('token');
+    window.location.reload();
   }
 }
 
