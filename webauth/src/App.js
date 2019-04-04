559e3a52f2cbb1e8cedc9110834289aa4ada18c8
JaySebagh <jaysebagh@gmail.com>
2019-04-04 13:55:51 -0700

    added routes

--- webauth/src/App.js
+++ webauth/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
 import './App.css';
 import Login from './Login/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login />
-      </div>
+      <>
+        <header>
+          <NavLink to="/">Home</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/login">Login</NavLink>
+        </header>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+        </main>
+      </>
     );
   }
+}
+
+function Home(props) {
+  return <h1>Home Component</h1>
 }
 
 export default App;
